62cfb727386678172bd7bce9ad31b53734b62eb3
eunyeoul <eunyull102@naver.com>
2020-03-12 08:40:49 +0900

    retrofit json to kotlin 추가

--- app/src/main/java/com/example/maskalarm/MainActivity.kt
+++ app/src/main/java/com/example/maskalarm/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Util.getHashKey(this)
-        
+
 
 
     }
---
+++ app/src/main/java/com/example/maskalarm/model/Stores.kt
@@ -0,0 +1,13 @@
+package com.example.maskalarm.model
+data class Stores (
+
+	val addr : String,
+	val code : Int,
+	val created_at : String,
+	val lat : Double,
+	val lng : Double,
+	val name : String,
+	val remain_stat : String,
+	val stock_at : String,
+	val type : Int
+)
